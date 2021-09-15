0abce5f43ccea1ca81ca3c40fe6a3f527ebe5e64
Krónos <lorenzo.chesi@live.it>
2021-09-15 18:50:16 +0200

    Update database.sql

--- sql/database.sql
+++ sql/database.sql
@@ -624,8 +624,126 @@ begin
 end $$
 delimiter ;
 
+-- Procedure Carica_Batteria_MANUAL #################################################################################################################
+Drop Procedure If Exists Carica_Batteria_MANUAL;
+Delimiter $$
+Create Procedure Carica_Batteria_MANUAL()
+    Begin
+    
+    Declare caricaBatteria int default 0;
+    Declare capienzaBatteria int default 0;
+    Declare Nrecord int default 0;
+    Declare TempProduzioneMenoConsumo int default 0;
+    Declare contatore int default 1;
+    
+    drop temporary table if exists risultati;
+    
+    create temporary table risultati (
+       ordine int not null default 0,
+       data_variazione datetime not null,
+       produzione_meno_consumo int not null default 0,
+    
+       primary key(ordine)
+    );
+    
+    insert into risultati
+       
+       with energia_target as
+       (
+          Select E.sorgente,
+                 E.data_variazione,
+                 E.Produzione,
+                 fascia_oraria_corrente(E.data_variazione) as id_fascia_oraria
+                 
+          From Energia E
+          Where E.data_variazione > now() - interval 80 minute
+       ),
+       logtable_produzione_intervallo as 
+       (
+          Select ET.sorgente,
+                 ET.data_variazione,
+                 ET.produzione as produzione_intervallo,
+                 IFO.batteria,
+                 IFO.uso_batteria
+          From energia_target ET
+               inner join
+               ImpostazioneFasciaOraria IFO
+               on IFO.id_fascia_oraria = ET.id_fascia_oraria
+       ),
+       produzione_sorgenti_e_consumo as
+       (
+          Select LPI.data_variazione,
+                 sum(LPI.produzione_intervallo) as produzione_totale_intervallo,
+                 LPI.batteria,
+                 LPI.uso_batteria,
+                 ConsumoRange(LPI.data_variazione,LPI.data_variazione + interval 30 minute) as consumo
+           From logtable_produzione_intervallo LPI
+           Group by LPI.data_variazione
+        )
+       
+       Select rank() over(order by PSC.data_variazione) as ordine,
+              PSC.data_variazione,
+              (PSC.produzione_totale_intervallo * PSC.batteria/100 - PSC.uso_batteria * PSC.consumo) as produzione_meno_consumo
+       From produzione_sorgenti_e_consumo PSC;
+     
+     Set caricaBatteria = 
+    (
+       Select sum(carica/100 * capienza)
+       From Batteria
+    );
+    
+    Set capienzaBatteria = 
+    (
+       Select sum(capienza)
+       From Batteria
+    );
+    
+    set Nrecord = 
+    (
+       Select count(*)
+       From risultati
+    );
+    
+    carica:loop
+       if contatore > Nrecord then
+       
+          leave carica;
+       
+       end if;
+       
+       Set TempProduzioneMenoConsumo =
+       (
+          Select produzione_meno_consumo
+          From risultati
+          Where ordine = contatore
+       );
+       
+       if caricaBatteria + TempProduzioneMenoConsumo >= capienzaBatteria then
+             
+          Set caricaBatteria = capienzaBatteria;
+          
+       elseif caricaBatteria + TempProduzioneMenoConsumo <= 0 then
+             
+          Set caricaBatteria = 0;
+          
+       else
+             
+          Set caricaBatteria = caricaBatteria + TempProduzioneMenoConsumo;
+          
+       end if;
+          
+       Set contatore = contatore + 1;
+       
+    end loop carica;
+    
+    Update Batteria
+    Set carica = capienza * caricaBatteria/capienzaBatteria;
+    
+end $$    
+delimiter ;
 
-
+drop event if exists CaricaBatteria;
+create event CaricaBatteria on schedule every 1 hour starts now() do call Carica_Batteria_MANUAL();
 
 
 -- ###########################################################################################################################################
@@ -1295,126 +1413,7 @@ begin
 end $$
 delimiter ;
 
--- Procedure Carica_Batteria_MANUAL #################################################################################################################
-Drop Procedure If Exists Carica_Batteria_MANUAL;
-Delimiter $$
-Create Procedure Carica_Batteria_MANUAL()
-    Begin
-    
-    Declare caricaBatteria int default 0;
-    Declare capienzaBatteria int default 0;
-    Declare Nrecord int default 0;
-    Declare TempProduzioneMenoConsumo int default 0;
-    Declare contatore int default 1;
-    
-    drop temporary table if exists risultati;
-    
-    create temporary table risultati (
-       ordine int not null default 0,
-       data_variazione datetime not null,
-       produzione_meno_consumo int not null default 0,
-    
-       primary key(ordine)
-    );
-    
-    insert into risultati
-       
-       with energia_target as
-       (
-          Select E.sorgente,
-                 E.data_variazione,
-                 E.Produzione,
-                 fascia_oraria_corrente(E.data_variazione) as id_fascia_oraria
-                 
-          From Energia E
-          Where E.data_variazione > now() - interval 80 minute
-       ),
-       logtable_produzione_intervallo as 
-       (
-          Select ET.sorgente,
-                 ET.data_variazione,
-                 ET.produzione as produzione_intervallo,
-                 IFO.batteria,
-                 IFO.uso_batteria
-          From energia_target ET
-               inner join
-               ImpostazioneFasciaOraria IFO
-               on IFO.id_fascia_oraria = ET.id_fascia_oraria
-       ),
-       produzione_sorgenti_e_consumo as
-       (
-          Select LPI.data_variazione,
-                 sum(LPI.produzione_intervallo) as produzione_totale_intervallo,
-                 LPI.batteria,
-                 LPI.uso_batteria,
-                 ConsumoRange(LPI.data_variazione,LPI.data_variazione + interval 30 minute) as consumo
-           From logtable_produzione_intervallo LPI
-           Group by LPI.data_variazione
-        )
-       
-       Select rank() over(order by PSC.data_variazione) as ordine,
-              PSC.data_variazione,
-              (PSC.produzione_totale_intervallo * PSC.batteria/100 - PSC.uso_batteria * PSC.consumo) as produzione_meno_consumo
-       From produzione_sorgenti_e_consumo PSC;
-     
-     Set caricaBatteria = 
-    (
-       Select sum(carica/100 * capienza)
-       From Batteria
-    );
-    
-    Set capienzaBatteria = 
-    (
-       Select sum(capienza)
-       From Batteria
-    );
-    
-    set Nrecord = 
-    (
-       Select count(*)
-       From risultati
-    );
-    
-    carica:loop
-       if contatore > Nrecord then
-       
-          leave carica;
-       
-       end if;
-       
-       Set TempProduzioneMenoConsumo =
-       (
-          Select produzione_meno_consumo
-          From risultati
-          Where ordine = contatore
-       );
-       
-       if caricaBatteria + TempProduzioneMenoConsumo >= capienzaBatteria then
-             
-          Set caricaBatteria = capienzaBatteria;
-          
-       elseif caricaBatteria + TempProduzioneMenoConsumo <= 0 then
-             
-          Set caricaBatteria = 0;
-          
-       else
-             
-          Set caricaBatteria = caricaBatteria + TempProduzioneMenoConsumo;
-          
-       end if;
-          
-       Set contatore = contatore + 1;
-       
-    end loop carica;
-    
-    Update Batteria
-    Set carica = capienza * caricaBatteria/capienzaBatteria;
-    
-end $$    
-delimiter ;
-
 -- Procedure InserimentoFasciaOraria ed Event EliminazioneFasciaOraria #################################################################################################################
-
 drop procedure if exists InserimentoFasciaOraria;
 delimiter $$
 create procedure InserimentoFasciaOraria (IN _OraInizio time,IN _OraFine time,IN  _Retribuzione int,IN _Prezzo int,IN _NomeUtente varchar(255),IN _Casa int,IN _Batteria int,IN _Rete int,IN _UsoBatteria tinyint)
